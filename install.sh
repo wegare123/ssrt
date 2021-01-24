acc31cbf4b8f37529a3c52bff9448cc3322d6ade
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-24 07:48:02 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#ssrt (Wegare)
+printf 'ctrl+c' | crontab -e > /dev/null
+opkg update && opkg install unzip
+cek=$(cat /etc/openwrt_r*)
+if [[ $cek == *"LEDE"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"aarch64_cortex-a53"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mips_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mipsel_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+else
+echo -e "version anda tidak terdeteksi!"
+exit
+fi
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/ssrt/main/ssrt.sh" -O /usr/bin/ssrt
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/ssrt/main/autorekonek-ssrt.sh" -O /usr/bin/autorekonek-ssrt
+cek=$(cat /etc/openwrt_r* | grep -i RELEASE | cut -d= -f2 | cut -d"'" -f2 | cut -d. -f1)
+if [ "$cek" = "19" ]; then
+wget --no-check-certificate "https://github.com/wegare123/ssrt/blob/main/shadowsocksr-libev_1-0-0-wegare_aarch64_cortex-a53.ipk?raw=true" -O ~/ssr-libev.ipk
+elif [ "$cek" = "18" ]; then
+wget --no-check-certificate "https://github.com/wegare123/ssrt/blob/main/shadowsocksr-libev_2.5.6-5_aarch64_cortex-a53.ipk?raw=true" -O ~/ssr-libev.ipk
+else
+echo "frimware versi yang anda gunakan tidak terdeteksi silahkan install sendiri shadowsocksr-libev nya"
+fi
+opkg install ip-full && opkg install *.ipk && opkg install lsof && opkg install fping
+chmod +x /usr/bin/ssrt
+chmod +x /usr/bin/autorekonek-ssrt
+rm -r ~/*.ipk
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/ssrt.conf
+touch ~/akun/ssrt.txt
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'ssrt'"
+
+				
---
+++ ssrt.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+#ssrt (Wegare)
+clear
+udp2="$(cat /root/akun/ssrt.txt | grep -i udp | cut -d= -f2)" 
+host2="$(cat /root/akun/ssrt.txt | grep -i host | cut -d= -f2 | head -n1)" 
+port2="$(cat /root/akun/ssrt.txt | grep -i port | cut -d= -f2)" 
+bug2="$(cat /root/akun/ssrt.txt | grep -i bug | cut -d= -f2)" 
+pass2="$(cat /root/akun/ssrt.txt | grep -i pass | cut -d= -f2)" 
+enc2="$(cat /root/akun/ssrt.txt | grep -i enc | cut -d= -f2)" 
+obfs2="$(cat /root/akun/ssrt.txt | grep -i obfs | cut -d= -f2)" 
+protocol2="$(cat /root/akun/ssrt.txt | grep -i protocol | cut -d= -f2)" 
+echo "Inject shadowsocksr by wegare"
+echo "1. Sett Profile"
+echo "2. Start Inject"
+echo "3. Stop Inject"
+echo "4. Enable auto booting & auto rekonek"
+echo "5. Disable auto booting & auto rekonek"
+echo "e. exit"
+read -p "(default tools: 2) : " tools
+[ -z "${tools}" ] && tools="2"
+if [ "$tools" = "1" ]; then
+
+echo "Masukkan host/ip" 
+read -p "default host/ip: $host2 : " host
+[ -z "${host}" ] && host="$host2"
+
+echo "Masukkan port" 
+read -p "default port: $port2 : " port
+[ -z "${port}" ] && port="$port2"
+
+echo "Masukkan pass" 
+read -p "default pass: $pass2 : " pass
+[ -z "${pass}" ] && pass="$pass2"
+
+echo "Masukkan bug" 
+read -p "default bug: $bug2 : " bug
+[ -z "${bug}" ] && bug="$bug2"
+
+read -p "ingin menggunakan port udpgw y/n " pilih
+if [ "$pilih" = "y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="--socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp"
+elif [ "$pilih" = "Y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="--socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp"
+else
+badvpn="--socks-server-addr 127.0.0.1:1080"
+fi
+
+echo "Masukkan encryption method" 
+read -p "default encryption method: $enc2 : " enc
+[ -z "${enc}" ] && enc="$enc2"
+
+echo "Masukkan protocol" 
+read -p "default protocol: $protocol2 : " protocol
+[ -z "${protocol}" ] && protocol="$protocol2"
+
+echo "Masukkan obfs" 
+read -p "default obfs: $obfs2 : " obfs
+[ -z "${obfs}" ] && obfs="$obfs2"
+
+echo "host=$host
+port=$port
+enc=$enc
+pass=$pass
+bug=$bug
+protocol=$protocol
+obfs=$obfs
+udp=$udp" > /root/akun/ssrt.txt
+cat <<EOF> /root/akun/ssrt.json
+{
+    "server":"$host",
+    "server_port":"$port",
+    "local_address":"127.0.0.1",
+    "local_port":1080,
+    "password":"$pass",
+    "timeout":120,
+    "method":"$enc",
+    "protocol":"$protocol",
+    "protocol_param":"",
+    "obfs":"$obfs",
+    "obfs_param":"$bug",
+    "redirect":"",
+    "dns_ipv6":false,
+    "fast_open":true,
+    "workers":1
+}
+EOF
+cat <<EOF> /usr/bin/gproxy-ssrt
+badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 $badvpn --udpgw-connection-buffer-size 65535 --udpgw-transparent-dns &
+EOF
+chmod +x /usr/bin/gproxy-ssrt
+
+echo "Sett Profile Sukses"
+sleep 2
+clear
+/usr/bin/ssrt
+elif [ "${tools}" = "2" ]; then
+ipmodem="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')" 
+echo "ipmodem=$ipmodem" > /root/akun/ipmodem.txt
+udp="$(cat /root/akun/ssrt.txt | grep -i udp | cut -d= -f2)" 
+host="$(cat /root/akun/ssrt.txt | grep -i host | cut -d= -f2 | head -n1)" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
+
+ssr-local -c /root/akun/ssrt.json &
+sleep 3
+ip tuntap add dev tun1 mode tun
+ifconfig tun1 10.0.0.1 netmask 255.255.255.0
+/usr/bin/gproxy-ssrt
+route add 8.8.8.8 gw "$route" metric 0
+route add 8.8.4.4 gw "$route" metric 0
+route add "$host" gw "$route" metric 0
+route add default gw 10.0.0.2 metric 0
+cat <<EOF> /usr/bin/ping-ssrt
+#!/bin/bash
+#ssrt (Wegare)
+while :
+do
+fping -c1 10.0.0.2
+sleep 1
+done
+EOF
+chmod +x /usr/bin/ping-ssrt
+/usr/bin/ping-ssrt > /dev/null 2>&1 &
+elif [ "${tools}" = "3" ]; then
+host="$(cat /root/akun/ssrt.txt | grep -i host | cut -d= -f2 | head -n1)" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
+#killall screen
+killall -q badvpn-tun2socks ssr-local ping-ssrt
+route del 8.8.8.8 gw "$route" metric 0 2>/dev/null
+route del 8.8.4.4 gw "$route" metric 0 2>/dev/null
+route del "$host" gw "$route" metric 0 2>/dev/null
+ip link delete tun1 2>/dev/null
+killall dnsmasq 
+/etc/init.d/dnsmasq start > /dev/null
+sleep 2
+echo "Stop Suksess"
+sleep 2
+clear
+/usr/bin/ssrt
+elif [ "${tools}" = "4" ]; then
+cat <<EOF>> /etc/crontabs/root
+
+# BEGIN AUTOREKONEKSSRT
+*/1 * * * *  autorekonek-ssrt
+# END AUTOREKONEKSSRT
+EOF
+sed -i 's/exit 0/ /g' /etc/rc.local
+/etc/init.d/cron restart
+echo "Enable Suksess"
+sleep 2
+clear
+/usr/bin/ssrt
+elif [ "${tools}" = "5" ]; then
+sed -i "/^# BEGIN AUTOREKONEKSSRT/,/^# END AUTOREKONEKSSRT/d" /etc/crontabs/root > /dev/null
+/etc/init.d/cron restart
+echo "Disable Suksess"
+sleep 2
+clear
+/usr/bin/ssrt
+elif [ "${tools}" = "e" ]; then
+clear
+exit
+else 
+echo -e "$tools: invalid selection."
+exit
+fi
